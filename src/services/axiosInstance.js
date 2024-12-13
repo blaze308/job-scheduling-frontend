d99887ba770cd35cb542744e5820270170067fab
blaze308 <jaimepinkrah@gmail.com>
2024-12-13 16:46:50 +0000

    third commit

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://job-scheduling-backend.onrender.com';
+
 const axiosInstance = axios.create({
     // baseURL: 'http://localhost:3000/api/meetings', // Change the baseURL to match your backend
-    baseURL: 'https://job-scheduling-backend.onrender.com/api/meetings', // Change the baseURL to match your backend
+    baseURL: `${API_BASE_URL}/api/meetings`, // Change the baseURL to match your backend
     headers: {
         'Content-Type': 'application/json',
     },
--- src/services/axiosUserInstance.js
+++ src/services/axiosUserInstance.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://job-scheduling-backend.onrender.com';
+
 const axiosUserInstance = axios.create({
     // baseURL: 'http://localhost:3000/api/users', // Replace with your backend URL
-    baseURL: 'https://job-scheduling-backend.onrender.com/api/users', // Replace with your backend URL
+    baseURL: `${API_BASE_URL}/api/meetings`, // Replace with your backend URL
     headers: {
         'Content-Type': 'application/json',
     },
 });
-
-export default axiosUserInstance;
